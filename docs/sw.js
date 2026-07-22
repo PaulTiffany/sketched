4fae0fe3b81047b1902cdee06f0c19c8dc94bb03
PaulTiffany <paulctiffany@gmail.com>
2026-07-22 10:37:44 -0400

    Add arXiv lowering to Z0 shell

--- docs/sw.js
+++ docs/sw.js
@@ -1,10 +1,11 @@
-const CACHE='z0-shell-v2';
-const SHELL=['./','./index.html','./manifest.webmanifest','./z0-icon.svg','./pdf.html','./pdf-support.js'];
+const CACHE='z0-shell-v3';
+const SHELL=['./','./index.html','./manifest.webmanifest','./z0-icon.svg','./pdf.html','./pdf-support.js','./arxiv-bridge.js'];
 const decorate=async response=>{
   const type=response.headers.get('content-type')||'';
   if(!type.includes('text/html'))return response;
   let html=await response.text();
   if(!html.includes('pdf-support.js'))html=html.replace('</body>','<script src="pdf-support.js"></script></body>');
+  if(!html.includes('arxiv-bridge.js'))html=html.replace('</body>','<script src="arxiv-bridge.js"></script></body>');
   const headers=new Headers(response.headers);
   headers.delete('content-length');
   return new Response(html,{status:response.status,statusText:response.statusText,headers});
